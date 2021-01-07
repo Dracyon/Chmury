d3c2b4240a390382b64702dfab82739375e98ef8
Wojciech Kowalczyk <w.kowalczyk@accenture.com>
2021-01-07 21:29:43 +0100

    add data and core projects, and sample inmemory list for displaying appointments

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AppoitmentWebApp.Data;
 
 namespace AppointmentWebApp
 {
@@ -29,6 +30,8 @@ namespace AppointmentWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IAppointmentData, InMemoryAppointmentData>();
+
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
 
--- Views/Appoitments/List.cshtml.cs
+++ Views/Appoitments/List.cshtml.cs
@@ -2,15 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppoitmentWebApp.Data;
+using AppoitmentWebApp.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AppointmentWebApp.Views.Appoitments
 {
     public class ListModel : PageModel
     {
+		private readonly IConfiguration config;
+		private readonly IAppointmentData appointmentData;
+
+		public string Message { get; set; }
+        public IEnumerable<Appointment> Appointments { get; set; }
+
+		public ListModel(IConfiguration config, IAppointmentData appointmentData)
+		{
+            this.config = config;
+            this.appointmentData = appointmentData;
+		}
+
         public void OnGet()
         {
+            Message = config["Message"];
+            Appointments = appointmentData.GetAll();
         }
     }
 }
